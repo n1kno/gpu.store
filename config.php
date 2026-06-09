a2b9a0b9f4b97e420834a4f40631b4e507e8ebcf
n1kno <125561616+n1kno@users.noreply.github.com>
2026-06-09 08:06:32 +0400

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $db_host = 'localhost';
 $db_user = 'root';
 $db_pass = '';
-$db_name = 'gpu_store';
+$db_name = 'gpu.store';
 
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 if ($conn->connect_error) {
